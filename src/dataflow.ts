b5017234bcad9b6ffa07323af02dbbe1d932212e
Peter Kelly <peter@pmkelly.net>
2018-03-27 03:18:40 +0700

    Dataflow: Add change propagation logic
    Keep track of the dependencies between different nodes in the dataflow
    graph. Each node has an array of inputs and outputs; the inputs are
    those other nodes on which this one depends, and the outputs are other
    nodes that depend on this one. The addOutput() and removeOutput()
    methods maintain these two arrays and are the only means by which they
    should be modified.
    
    Note that the term "input" here is distinct from its usage as a location
    whose value can be changed by the runtime system externally from the
    program itself.
    
    Rudimentary tracing facilities are included to report which nodes are
    changed, so that change propagation can be observed. Each dataflow node
    is given a unique identifier for use in this and future debugging
    facilities.
    
    The DataflowNode class now has two abstract methods, reevaluate() and
    detach(). The former is called when the value of one or more inputs has
    changed, while the latter is intended to be called when it is known that
    the node is no longer needed and will be removed from the graph.

--- src/dataflow.ts
+++ src/dataflow.ts
@@ -39,6 +39,20 @@ import {
 } from "./ast";
 
 const allInputs = new Map<string, InputDataflowNode>();
+let dirtyNodes: DataflowNode[] = [];
+
+export function reevaluateDataflowGraph(): void {
+    while (true) {
+        const nodes = dirtyNodes;
+        dirtyNodes = [];
+        if (nodes.length === 0)
+            break;
+        for (const node of nodes) {
+            node.dirty = false;
+            node.reevaluate();
+        }
+    }
+}
 
 export function createInput(name: string, initialValue: Value): void {
     const existing = allInputs.get(name);
@@ -63,12 +77,96 @@ export function updateInput(name: string, value: Value): void {
 }
 
 export abstract class DataflowNode {
+    private static nextId = 0;
+    private id: number;
     public value: Value = UnspecifiedValue.instance;
+    private inputs: DataflowNode[] = [];
+    private outputs: DataflowNode[] = [];
+    public dirty: boolean = false;
+
+    public constructor() {
+        this.id = DataflowNode.nextId++;
+    }
+
+    public addOutput(output: DataflowNode): void {
+        const outputIndex = this.outputs.indexOf(output);
+        if (outputIndex >= 0)
+            throw new Error("addOutput: output is already present");
+        this.outputs.push(output);
+
+        const inputIndex = output.inputs.indexOf(this);
+        if (inputIndex >= 0)
+            throw new Error("addOutput: input is already present");
+        output.inputs.push(this);
+    }
+
+    public removeOutput(output: DataflowNode): void {
+        const outputIndex = this.outputs.indexOf(output);
+        if (outputIndex < 0)
+            throw new Error("removeOutput: output is not present in list");
+        this.outputs.splice(outputIndex, 1);
+
+        const inputIndex = output.inputs.indexOf(this);
+        if (inputIndex < 0)
+            throw new Error("removeOutput: input is not present in list");
+        output.inputs.splice(inputIndex, 1);
+    }
+
+    public markDirty(): void {
+        if (!this.dirty) {
+            this.dirty = true;
+            dirtyNodes.push(this);
+        }
+    }
+
+    protected markOutputsAsDirty(): void {
+        for (const output of this.outputs)
+            output.markDirty();
+    }
+
+    public updateValue(newValue: Value): void {
+        if (this.value !== newValue) {
+            this.trace("value changed: " + this.value + " -> " + newValue);
+            this.value = newValue;
+            this.markOutputsAsDirty();
+        }
+        // else {
+        //     this.trace("value unchanged: " + this.value);
+        // }
+    }
+
+    public toString(): string {
+        const fullName = "" + (<any> this).constructor.name;
+        const name = fullName.replace(/DataflowNode$/, "");
+        return name + "[" + this.id + "]";
+    }
+
+    protected trace(msg: string): void {
+        console.log(this + " " + msg);
+    }
+
+    // Called when the change propagation logic has determined that one or more inputs to this
+    // node have changed their value. The node should re-execute whatever operations it originally
+    // performed to obtain its own value, but retaining existing dataflow nodes where appropriate.
+    public abstract reevaluate(): void;
+
+    // Called when the node is no longer, and will never again be part of the dataflow graph. The
+    // purpose of this method is to ensure that there are no outstanding references to it, so that
+    // it may be garbage collected.
+    public abstract detach(): void;
 }
 
 export class EnvSlotDataflowNode extends DataflowNode {
     public constructor() {
         super();
+    }
+
+    public reevaluate(): void {
+        // Nothing to do here
+    }
+
+    public detach(): void {
+        // Nothing to do here
     }
 }
 
@@ -77,6 +175,14 @@ export class ConstantDataflowNode extends DataflowNode {
         super();
 
         this.value = this.ast.value.toValue();
+    }
+
+    public reevaluate(): void {
+        // Nothing to do here
+    }
+
+    public detach(): void {
+        // Nothing to do here
     }
 }
 
@@ -88,6 +194,14 @@ export class AssignDataflowNode extends DataflowNode {
         const variable = this.env.resolveRef(this.ast.ref, this.ast.range);
         variable.value = value;
         this.value = UnspecifiedValue.instance;
+    }
+
+    public reevaluate(): void {
+        throw new Error("AssignDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("AssignDataflowNode.reevaluate() not implemented");
     }
 }
 
@@ -101,6 +215,14 @@ export class IfDataflowNode extends DataflowNode {
         else
             this.value = this.ast.alternative.createDataflowNode(this.env).value;
     }
+
+    public reevaluate(): void {
+        throw new Error("IfDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("IfDataflowNode.reevaluate() not implemented");
+    }
 }
 
 export class LambdaDataflowNode extends DataflowNode {
@@ -108,6 +230,14 @@ export class LambdaDataflowNode extends DataflowNode {
         super();
 
         this.value = new LambdaProcedureValue(this.env, this.ast);
+    }
+
+    public reevaluate(): void {
+        throw new Error("LambdaDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("LambdaDataflowNode.reevaluate() not implemented");
     }
 }
 
@@ -117,6 +247,14 @@ export class SequenceDataflowNode extends DataflowNode {
 
         this.ast.body.createDataflowNode(this.env);
         this.value = this.ast.next.createDataflowNode(this.env).value;
+    }
+
+    public reevaluate(): void {
+        throw new Error("SequenceDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("SequenceDataflowNode.reevaluate() not implemented");
     }
 }
 
@@ -160,6 +298,14 @@ export class ApplyDataflowNode extends DataflowNode {
         const node = procValue.proc.body.createDataflowNode(innerEnv);
         return node.value;
     }
+
+    public reevaluate(): void {
+        throw new Error("ApplyDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("ApplyDataflowNode.reevaluate() not implemented");
+    }
 }
 
 export class VariableDataflowNode extends DataflowNode {
@@ -168,6 +314,14 @@ export class VariableDataflowNode extends DataflowNode {
 
         const variable = this.env.resolveRef(this.ast.ref, this.ast.range);
         this.value = variable.value;
+    }
+
+    public reevaluate(): void {
+        throw new Error("VariableDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("VariableDataflowNode.reevaluate() not implemented");
     }
 }
 
@@ -182,11 +336,27 @@ export class LetrecDataflowNode extends DataflowNode {
         bindLetrecValues(bindingArray, this.ast, innerEnv);
         this.value = this.ast.body.createDataflowNode(innerEnv).value;
     }
+
+    public reevaluate(): void {
+        throw new Error("LetrecDataflowNode.reevaluate() not implemented");
+    }
+
+    public detach(): void {
+        throw new Error("LetrecDataflowNode.reevaluate() not implemented");
+    }
 }
 
 export class InputDataflowNode extends DataflowNode {
     public constructor(initialValue: Value) {
         super();
         this.value = initialValue;
+    }
+
+    public reevaluate(): void {
+        // Nothing to do here
+    }
+
+    public detach(): void {
+        // Nothing to do here
     }
 }
